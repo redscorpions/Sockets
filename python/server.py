9293a56b85218ead2047be6d7c4dc755d94065a9
redscorpions <66802298+redscorpions@users.noreply.github.com>
2024-03-19 22:37:45 +0100

    Create server.py

---
+++ python/server.py
@@ -0,0 +1,52 @@
+import socket # libreria per la gestione dei socket
+import termcolor # libreria per la gestione dei colori del terminale
+
+def server_start():
+    valida = True
+    port = 27015 # porta predefinita per la connessione
+    
+    while(valida):
+        # Scegliere se inizializzare una connessione basata su protocollo UDP o TCP
+        print('Seleziona opzione:\n1) Avvia il server in loopback\n2) Avvia il server in rete locale')
+        opzione = int(input(' -> ')) # scelta
+        while(opzione != 1 or opzione != 2): # loop per assicurarsi che la scelta sia valida
+            opzione = int(input(' -> '))
+
+        if opzione == 1:
+            host = lambda x: socket.gethostbyname("localhost") # ottenimento dell'indirizzo IP di loopback (localhost)
+        elif opzione == 2:
+            socket.gethostbyname(socket.gethostname()) # ottenimento dell'indirizzo IP su rete locale
+    
+    server_socket = socket.socket() # creazione del nuovo socket
+
+    server_socket.bind((host, port)) # associazione dell'indirizzo IP e della porta al socket
+
+    server_socket.listen(1) # limitazione ad un solo client che può connettersi
+
+    print(termcolor.colored(("[+] Server avviato sull'indirizzo IP: " + host + ':' + str(port)), 'green')) # comunicazione di avvenuto avviamento del server socket
+
+    conn, address = server_socket.accept() # accettazione delle connessioni
+    print(termcolor.colored(("[+] Connessione da: " + str(address[0]) + ':' + str(address[1])), 'green')) # visualizzazione delle informazioni del client connesso
+
+    # rimane in ascolto
+    while(True):
+        # riceve dati con dimensione massima del buffer impostata a 1024 byte
+        data = conn.recv(1024).decode()
+
+        # interrompe il loop se non vengono più ricevuti dati
+        if not data:
+            break
+
+        print("Ricevuto: " + str(data)) # stampa del messaggio ricevuto
+
+        data = input(' -> ') # richiesta di una risposta da inserire sul server
+        
+        conn.send(data.encode()) # invio della risposta al Client
+    
+    print(termcolor.colored(('[x] Chiusura connessione'), 'red')) # avviso di chiusura connessione
+
+    conn.close() # chiusura della connessione
+
+# la funzione viene chiamata solo se il file viene eseguito come main
+if __name__ == '__main__':
+    server_start()
